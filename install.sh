c2bb9692f17d6ed789292fbb80259009495022fa
mack-a <mack-a>
2019-11-06 17:30:36 +0800

    feat(配置文件、一键脚本): 修改配置文件中域名部分、完善一键脚本

--- install.sh
+++ install.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 export PATH="/usr/bin/:#$PATH"
-nginxStatus=false
-v2rayStatus=false
-httpsStatus=false
+
 # todo 先完善正常步骤
-initNginx(){
+installNginx(){
     echo -e '\033[36m   检查Nginx中... \033[0m'
     existProcessNginx=`ps -ef|grep nginx|grep -v grep`
     existNginx=`command -v nginx`
     if [ -z "$existProcessNginx" ] && [ -z "$existNginx" ]
     then
         echo '安装Nginx中，如遇到是否安装输入y'
-        yum update
         yum install nginx
         echo '步骤二：Nginx安装成功，执行下一步'
-        installV2Ray
     else
         # todo
-        echo '检查到Nginx存在，是否停止并卸载，输入y/Y确认：'
+        echo '检查到Nginx存在，是否停止并卸载，输入y确认：'
         read -e unstallStatus
         if [[ $unstallStatus -eq "y" ||  $unstallStatus -eq "Y" ]]
         then
@@ -28,22 +24,111 @@ initNginx(){
     fi
 }
 installHttps(){
-    echo 'https'
+    echo '安装https中,请输入你要生成tls证书的域名'
+    read -e domain
+    curl https://get.acme.sh | sh
+    sudo ~/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+    ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/nginx/$domain.crt --keypath /etc/nginx/$domain.key --ecc
 }
 installV2Ray(){
     echo -e '\033[36m   检查V2Ray中... \033[0m'
-
+    existProcessV2Ray=`ps -ef|grep v2ray|grep -v grep`
+    existV2Ray=`command -v v2ray`
+    if [ -z "$existProcessV2Ray" ] && [ -z "$existV2Ray" ]
+    then
+        echo '安装V2Ray中...'
+        wget -P /tmp/v2ray https://github.com/v2ray/v2ray-core/releases/download/v4.21.3/v2ray-linux-64.zip
+        cd /tmp/v2ray
+        unzip /tmp/v2ray/v2ray-linux-64.zip
+        mv /tmp/v2ray/v2ray /usr/bin/
+        mv /tmp/v2ray/v2ctl /usr/bin/
+        echo '步骤二：V2Ray安装成功，执行下一步'
+    else
+        # todo
+        echo '检查到V2Ray存在，是否停止并卸载，输入y确认：'
+        read -e unstallStatus
+        if [[ $unstallStatus -eq "y" ||  $unstallStatus -eq "Y" ]]
+        then
+            echo '卸载'
+        else
+            echo '不卸载，停止脚本'
+        fi
+    fi
 }
 checkOS(){
     systemVersion=`cat /etc/redhat-release|grep CentOS|awk '{print $1}'`
     if [ -n "$systemVersion" ] && [ "$systemVersion" == "CentOS" ]
     then
-        echo ''
         echo -e '\033[35m步骤一：系统为CentOS，执行下一步 \033[0m'
-        return 1
     else
         echo '目前仅支持Centos'
     fi
+}
+installTools(){
+    existProcessWget=`ps -ef|grep wget|grep -v grep`
+    existWget=`command -v wget`
+    yum update
+    if [ -z "$existProcessWget" ] && [ -z "$existWget" ]
+    then
+        echo '安装wget中...'
+        yum install wget
+    else
+        echo
+    fi
+    existUnzip=`command -v unzip`
+    if [ -z "$existUnzip" ]
+    then
+        echo '安装zip中...'
+        yum install unzip
+    else
+        echo
+    fi
+    existSocat=`command -v socat`
+    if [ -z "$existSocat" ]
+    then
+        echo '安装zip中...'
+        yum install socat
+    else
+        echo
+    fi
+}
+manageFun(){
+    case $1 in
+        checkOS)
+            checkOS
+        ;;
+        installNginx)
+            installNginx
+        ;;
+        installV2Ray)
+            installV2Ray
+        ;;
+        installHttps)
+            installHttps
+        ;;
+    esac
+}
+automationFun(){
+    case $1 in
+        1)
+            checkOS
+            installTools
+            automationFun 2
+        ;;
+        2)
+            installNginx
+            automationFun 3
+        ;;
+        3)
+            installV2Ray
+            automationFun 4
+        ;;
+        4)
+            installHttps
+            echo '安装完毕'
+            exit
+        ;;
+    esac
 }
 init(){
     echo -e "\033[35m此脚本会执行以下内容: \033[0m"
@@ -57,8 +142,15 @@ init(){
     then
         echo '手动模式'
     else
-        checkOS
-        echo "$?"
+        automationFun 1
+
+        if [ $? = 1 ]
+        then
+            manageFun installNginx
+        else
+            echo '退出脚本'
+            exit
+        fi
     fi
 }
 init
