09086ec8c98760c7744c9a2c9d738f6ff481d8a4
mack-a <jerrymattmeng@gmail.com>
2019-12-13 16:25:23 +0800

    feat(脚本): 增加系统判断

--- install.sh
+++ install.sh
@@ -9,6 +9,41 @@ magenta="\e[95m"
 cyan="\e[96m"
 none="\e[0m"
 
+# 检查系统
+checkSystem(){
+#	if [[ -f /etc/redhat-release ]]; then
+#		release="centos"
+#	elif cat /etc/issue | grep -q -E -i "debian"; then
+#		release="debian"
+#	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+#		release="ubuntu"
+#	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+#		release="centos"
+#	elif cat /proc/version | grep -q -E -i "debian"; then
+#		release="debian"
+#	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+#		release="ubuntu"
+#	el
+	if cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+}
+
+#检查Linux版本
+check_version(){
+	if [[ -s /etc/redhat-release ]]; then
+		version=`grep -oE  "[0-9.]+" /etc/redhat-release | cut -d . -f 1`
+	else
+		version=`grep -oE  "[0-9.]+" /etc/issue | cut -d . -f 1`
+	fi
+	bit=`uname -m`
+	if [[ ${bit} = "x86_64" ]]; then
+		bit="x64"
+	else
+		bit="x32"
+	fi
+}
+
 installNginx(){
     echo -e "${skyBlue}检查Nginx中...${none} "
     existProcessNginx=`ps -ef|grep nginx|grep -v grep`
@@ -401,5 +436,8 @@ init(){
         manageFun
     fi
 }
+checkSystem
+[[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
 init
+# [[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] &&
 # generatorVmess
